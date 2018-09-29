a91b4827a34506a91b855495aae3900781adaa27
Leonardo Luz <leo.luz14@hotmail.com>
2018-09-29 19:59:36 -0300

    projeto iniciado com Hello World

---
+++ GameXadres/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameXadres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
