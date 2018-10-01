63ec01f208fb70bc661f6e2e3c316f3a9ec8d682
Leonardo Luz <leo.luz14@hotmail.com>
2018-10-01 12:14:55 -0300

    Implementando validações

---
+++ GameXadres/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameXadres.tabuleiro
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
